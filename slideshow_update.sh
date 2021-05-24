8f7a5bf69ff290c89397a770ba4284f185251e97
Phil Burr <phil.burr@gmail.com>
2021-05-24 14:53:05 -0600

    Fix for metadata

--- slideshow_update.sh
+++ slideshow_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pi/slideshow
 git clean -fd
-git pull 
+git pull
 
 pgrep -f "python3 slideshow.py" | xargs kill
 DISPLAY=:0.0 python3 slideshow.py /home/pi/slideshow/photos 12 &
--- slideshow_window.py
+++ slideshow_window.py
@@ -12,6 +12,7 @@ from gi.repository import GExiv2, GLib
 from google_photos_sync import GooglePhotos
 from slideshow_frame import SlideshowFrame
 
+from PIL import Image, IptcImagePlugin
 import exifread
 import xml.etree.ElementTree as ET
 
@@ -55,6 +56,32 @@ class MainWindow(QMainWindow):
 
         self.updateAlbum()
 
+    def read_metadata(self, filename):
+        try:
+            image = Image.open(filename)
+            iptc = IptcImagePlugin.getiptcinfo(image)
+            metadata = {k.strip():v.strip() for (k,v) in [kv.decode("utf-8").split(':') for kv in iptc[(2, 25)]]}
+            return metadata
+        except Exception as ex:
+            try:
+                tags = None
+                with open(filename, 'rb') as image_file:
+                    tags = exifread.process_file(image_file, debug=True)
+
+                if tags and 'Image ApplicationNotes' in tags:
+                    xml = str(tags['Image ApplicationNotes'])
+                    xml = ET.fromstring(xml)
+                    items = xml.findall('.//{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Bag/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}li')
+                    items = [item.text.split(':') for item in items]
+                    items = [(a.strip(), b.strip()) for a, b in items]
+                    metadata = dict(items)
+                    return metadata
+
+            except Exception as ex2:
+                pass
+
+        return None
+
     def scan(self):
         new_files = []
         for r, dirs, files in os.walk(os.path.abspath(self.root)):
@@ -65,20 +92,9 @@ class MainWindow(QMainWindow):
         metadata = {}
         for fname in new_files:
             try:
-                tags = None
-                with open(fname, 'rb') as image_file:
-                    tags = exifread.process_file(image_file, debug=True)
-
-                if tags:
-                    if 'Image ApplicationNotes' in tags:
-                        xml = str(tags['Image ApplicationNotes'])
-                        xml = ET.fromstring(xml)
-                        items = xml.findall('.//{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Bag/{http://www.w3.org/1999/02/22-rdf-syntax-ns#}li')
-                        items = [item.text.split(':') for item in items]
-                        items = [(a.strip(), b.strip()) for a, b in items]
-                        md = dict(items)
-                        if 'Name' in md:
-                            metadata[fname] = md
+                md = self.read_metadata(fname)
+                if 'Name' in md:
+                    metadata[fname] = md
             except:
                 pass
 
